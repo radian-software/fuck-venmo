5e65fa19d617d67004e467789f145332c043dd84
Radon Rosborough <radon@intuitiveexplanations.com>
2024-03-02 09:05:36 -0800

    start working on sending emails

--- fuck_venmo/fastmail.py
+++ fuck_venmo/fastmail.py
@@ -49,7 +49,12 @@ class Fastmail:
         responses = []
         for status, contents, _ in responses_struct:
             if status == "error":
-                raise JMAPError(contents["type"] + ": " + contents["description"])
+                try:
+                    raise JMAPError(contents["type"] + ": " + contents["description"])
+                except KeyError:
+                    raise JMAPError(
+                        contents.pop("type") + ": " + repr(contents)
+                    ) from None
             responses.append(contents)
         if len(responses) == 1 and not force_multiple:
             return responses[0]
@@ -111,3 +116,95 @@ class Fastmail:
                 collected_content["textBody"] or collected_content["htmlBody"]
             )
         return emails["list"]
+
+    def send_email(
+        self,
+        from_name: str,
+        from_email: str,
+        to_name: str,
+        to_email: str,
+        subject: str,
+        body_text: str,
+        reply_to_id: str = "",
+    ):
+        drafts_folders, sent_folders = self._call(
+            Query(
+                "Mailbox/query",
+                {"filter": {"role": "drafts"}},
+            ),
+            Query("Mailbox/query", {"filter": {"role": "sent"}}),
+        )
+        (drafts_folder,) = drafts_folders["ids"]
+        (sent_folder,) = sent_folders["ids"]
+        self._call(
+            Query(
+                "Identity/get", {"filter": {"email": from_email}}, name="get_identity"
+            ),
+            Query(
+                "Email/set",
+                {
+                    "create": {
+                        "draft_id": {
+                            "mailboxIds": {
+                                drafts_folder: True,
+                            },
+                            "keywords": {"$seen": True, "$draft": True},
+                            "from": [
+                                {
+                                    "name": from_name,
+                                    "email": from_email,
+                                }
+                            ],
+                            "to": [
+                                {
+                                    "name": to_name,
+                                    "email": to_email,
+                                }
+                            ],
+                            "subject": subject,
+                            "bodyStructure": {
+                                "type": "text/plain",
+                                "partId": "body_id",
+                            },
+                            "bodyValues": {
+                                "body_id": {"value": body_text, "isTruncated": False}
+                            },
+                        }
+                    }
+                },
+            ),
+            Query(
+                "EmailSubmission/set",
+                {
+                    "create": {
+                        "sent_id": {
+                            "#identityId": {
+                                "resultOf": "get_identity",
+                                "name": "Identity/get",
+                                "path": "/id",
+                            },
+                            "emailId": "#draft_id",
+                            "envelope": {
+                                "mailFrom": {
+                                    "email": from_email,
+                                    "parameters": None,
+                                },
+                                "rcptTo": [
+                                    {
+                                        "email": to_email,
+                                        "parameters": None,
+                                    }
+                                ],
+                            },
+                        }
+                    },
+                    "onSuccessUpdateEmail": {
+                        "#sent_id": {
+                            drafts_folder: None,
+                            sent_folder: True,
+                            "keywords/$draft": None,
+                        }
+                    },
+                },
+            ),
+        )
